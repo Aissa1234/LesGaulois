9eb74dbed7b012a0db85c0c9a04ce0a537c6ae10
Aissa1234 <81842422+Aissa1234@users.noreply.github.com>
2022-10-18 09:20:50 +0200

    Update Village.java

--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -1,20 +1,39 @@
 package personnages;
 
 public class Village {
+	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois=0;
+
+	public Village(String nom,int nbVillageoisMax) {
+		this.nom = nom;
+		this.villageois = new Gaulois[nbVillageoisMax];
+	}
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
+	}
 	
-private String nom;
-private Chef chef;
-private Gaulois[]Villageois;
-privte int numVillageois;
-public Village(String nom) {
-this.nom = nom;
-}
-public void setChef(Chef chef) {
-this.chef = chef;
-}
-public String getNom() {
-return nom;
-}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void ajouterVillageois(Gaulois g) {
+		villageois[nbVillageois] = g;
+		nbVillageois ++;
+	}
+	
+	public Gaulois trouverHabitant(int n) {
+		return villageois[n];
+	}
+	
+	public void afficherVillageois() {
+		System.out.println("Dans le village du chef " + chef.getNom() + " habitent :");
+		for (int i = 0;i<nbVillageois;i++) {
+			System.out.println(villageois[i].getNom());
+		
 
-
+}
+}
 }
